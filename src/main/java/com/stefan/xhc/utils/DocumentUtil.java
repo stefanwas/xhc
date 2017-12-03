2e6a69e6f7d93c726bd837c0f655b0b285b218a6
stefanwas <swtemp001@gmail.com>
2017-12-04 00:30:31 +0100

    added document util - improved

--- src/main/java/com/stefan/xhc/utils/DocumentUtil.java
+++ src/main/java/com/stefan/xhc/utils/DocumentUtil.java
@@ -11,22 +11,18 @@ public class DocumentUtil {
     private static final ObjectMapper mapper = new ObjectMapper();
 
     public static Document deserialize(String json) {
-        Document document = null;
         try {
-            document = mapper.readValue(json, Document.class);
+            return mapper.readValue(json, Document.class);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        return document;
     }
 
     public static String serialize(Document document) {
-        String json = null;
         try {
-            json = mapper.writeValueAsString(document);
+            return mapper.writeValueAsString(document);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
-        return json;
     }
 }
